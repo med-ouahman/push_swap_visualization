2a475e5b1321e5f320a78fc86c3c937d42bceef4
Mohamed Ouahmane <mouahman@c2r6p1.1337.ma>
2025-01-17 20:19:16 +0100

    ppp

--- main.js
+++ main.js
@@ -13,9 +13,110 @@ const ss = document.getElementById("ss");
 const rr = document.getElementById("rr");
 const rrr = document.getElementById("rrr");
 const instructionEl = document.getElementById("instructions");
+const insertBtn = document.getElementById("insert-operations");
 
 const INT_MAX = 2147483647;
 const INT_MIN = -2147483648;
+
+const operationMap = {
+	"pa": doPa,
+	"pb": doPb,
+	"sa": doSa,
+	"sb": doSb,
+	"ra": doRa,
+	"rb": doRb,
+	"rra": doRra,
+	"rrb": doRrb,
+	"ss": doSs,
+	"rr": doRr,
+	"rrr": doRrr,
+}
+
+function doPa() {
+	if (stackB.length)
+		printInstructions("pa");
+	push(stackB, stackA);
+	fillStack(stackB, stackBEl);
+	fillStack(stackA, stackAEl);
+}
+
+function doPb() {
+	if (stackA.length)
+		printInstructions("pb");
+	push(stackA, stackB);
+	fillStack(stackA, stackAEl);
+	fillStack(stackB, stackBEl);
+}
+
+function doSa() {
+	swap(stackA);
+	fillStack(stackA, stackAEl);
+	if (stackA.length > 1)
+		printInstructions("sa");
+}
+
+function doSb() {
+	swap(stackB);
+	fillStack(stackB, stackBEl);
+	if (stackB.length > 1)
+		printInstructions("sb");
+}
+
+function doRa() {
+	rotate(stackA);
+	fillStack(stackA, stackAEl);
+	if (stackA.length > 1)
+		printInstructions("ra");
+}
+
+function doRb() {
+	rotate(stackB);
+	fillStack(stackB, stackBEl);
+	if (stackB.length > 1)
+		printInstructions("rb");
+}
+
+function doRra() {
+	reverseRotate(stackA);
+	fillStack(stackA, stackAEl);
+	if (stackA.length > 1)
+		printInstructions("rra");
+}
+
+function doRrb() {
+	reverseRotate(stackB);
+	fillStack(stackB, stackBEl);
+	if (stackB.length > 1)
+		printInstructions("rrb");
+}
+
+function doRrr() {
+	reverseRotate(stackA);
+	reverseRotate(stackB);
+	fillStack(stackA, stackAEl);
+	fillStack(stackB, stackBEl);
+	if (stackA.length > 1 || stackB.length > 1)
+		printInstructions("rrr");
+}
+
+function doSs() {
+	swap(stackA);
+	swap(stackB);
+	fillStack(stackA, stackAEl);
+	fillStack(stackB, stackBEl);
+	if (stackA.length > 1 || stackB.length > 1)
+		printInstructions("ss");
+}
+
+function doRr() {
+	rotate(stackA);
+	rotate(stackB);
+	fillStack(stackA, stackAEl);
+	fillStack(stackB, stackBEl);
+	if (stackA.length > 1 || stackB.length > 1)
+		printInstructions("rr");
+}
+
 let inputs = "", stackA = [], stackB = [], instructions = 0;
 do
 {
@@ -155,7 +256,7 @@ rr.addEventListener("click", () => {
 	rotate(stackA);
 	rotate(stackB);
 	fillStack(stackA, stackAEl);
-	fillStack(stackB, stackBEl);
+	fillStack(stackB, stackBEl);9
 	if (stackA.length > 1 || stackB.length > 1)
 		printInstructions("rr");
 });
@@ -164,7 +265,7 @@ ss.addEventListener("click", () => {
 	swap(stackA);
 	swap(stackB);
 	fillStack(stackA, stackAEl);
-	fillStack(stackB, stackBEl);
+	fillStack(stackB, stackBEl);is_sorted
 	if (stackA.length > 1 || stackB.length > 1)
 		printInstructions("ss");
 });
@@ -177,3 +278,20 @@ rrr.addEventListener("click", () => {
 	if (stackA.length > 1 || stackB.length > 1)
 		printInstructions("rrr");
 });
+
+function insertOperations() {
+	const operations = ["pa", "pb", "sa", "sb", "ra", "rb", "rra", "rrb", "ss", "rr", "rrr"];
+	let inputs, stack = [];
+	do {
+		inputs = prompt("Enter Operations:");
+		stack = inputs.split(" ");
+	} while(stack.forEach(operation => !operations.includes(operation)));
+
+	stack.forEach(execute);
+}
+
+function execute(operation) {
+	setTimeout(operationMap[operation], 0);
+}
+
+insertBtn.addEventListener("click", insertOperations);
